608aa59945c6d3369cf89e21989c020af9a48a46
christinemkhachen <christinemkhachen@gmail.com>
2021-05-07 17:00:13 -0400

    second

---
+++ index.js
@@ -0,0 +1,31 @@
+function addingEventListener() {
+
+    const input = document.getElementById('click');
+
+    
+
+ 
+
+    function clickAlert() {
+
+  alert(`Hope you enjoy scrolling through the last 8 decades of fashion!`);
+
+}
+
+ 
+
+input.addEventListener('click', clickAlert); 
+
+ 
+
+}
+
+ 
+
+addingEventListener();
+
+ 
+
+ 
+
+ 
